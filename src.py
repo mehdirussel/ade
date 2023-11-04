893ee164dbe393eb1817674941bbaaf5baa844e1
mehdirussel <35711089+mehdirussel@users.noreply.github.com>
2023-11-04 17:25:15 +0100

    Update src.py

--- src.py
+++ src.py
@@ -8,7 +8,6 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.common.exceptions import StaleElementReferenceException,ElementClickInterceptedException,TimeoutException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
